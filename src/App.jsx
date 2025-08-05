95a749ce8daf0d7856b34841e6f14c0411f441d0
VarshithaKolusu <varshithakolusu2006@gmail.com>
2025-08-05 11:52:28 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       {/* NEW TEXT */}
-      <h2>CICD section 211</h2>
+      <h2>CICD section 211 !</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
